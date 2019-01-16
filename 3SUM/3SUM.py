dda925fe3552716622f98f9036edbdb464d57cb1
Angelos Guan <angelosguan@gmail.com>
2019-01-15 22:03:10 -0600

    3SUM finished

--- 3SUM/3SUM.py
+++ 3SUM/3SUM.py
@@ -1,24 +1,51 @@
-"""Takes a sequence of number as argument
+"""Takes a sum result and a sequence of number as argument
    print out number of such pairs and the pairs
 """
 import sys
-import solution.py as soln
+from solution import solve_3SUM
 
 raw_in = sys.argv
+try:
+	res = int(sys.argv[1])
+except:
+	print("enter integer sum")
+	sys.exit
+
 del raw_in[0]
+del raw_in[1]
 arr = []
 
 for elem in raw_in:
 	try:
 		num = int(elem)
 		arr.append(num)
-		break
 	except:
 		print("enter number sequence")
 		sys.exit
 
-ans = soln.solve_3SUM(arr)
+ans = solve_3SUM(arr,res)
 
-print("total number is: " + len(ans)+".")
-print(ans)
+k = len(ans)
+
+if k==0:
+	print("No answer found.")
+else:
+	for i in range(0,k):
+		a = ans[i][0]
+		b = ans[i][1]
+		c = ans[i][2]
+		if (a+b+c != res):
+			print("solution wrong")
+			sys.exit()
+		for j in range(i+1, k):
+			other = ans[j]
+			if ((a,b,c)==other) or ((a,c,b)==other) or ((b,a,c)==other) or ((b,c,a)==other) or ((c,a,b)==other) or ((c,b,a)==other):
+				print("solution has duplicate")
+				print(ans)
+				sys.exit()
+
+	print("total number is: " + str(k) + ".")
+	print(ans)
+
+
 
--- 3SUM/solution.py
+++ 3SUM/solution.py
@@ -1,6 +1,46 @@
 import numpy as np
 
-def solve_3SUM(arr):
+# Takes in a sequence and a sum to find
+# iterate through the arr without duplicate, then iterate from start and end of right side
+# if sum of two numbers > desired, move back right pointer; else move forward left pointer
+# remove duplicate of left and right pointer
+# result should be unique then
+
+def solve_3SUM(arr,res):
+	l = len(arr)
+	if l < 3:
+		return []
+
+	arr.sort()
+
 	ans = []
+
+	i = 0
+	while i < l:
+
+		if(float(arr[i])>res/3.0):
+			return ans
+
+		target = res - arr[i]
+		left = i + 1
+		right  = l - 1
+		while (left < right):
+			total = arr[left] + arr[right]
+
+			if total > target:
+				right -= 1
+			elif total < target:
+				left += 1
+			else:
+				a = arr[i]
+				b = arr[left]
+				c = arr[right]
+				ans.append( (a, b, c) )
+				while(left < right and arr[left]==b):
+					left += 1
+				while(left < right and arr[right]==c):
+					right -= 1
+		while ( i+1 < l and arr[i] == res-target):
+			i += 1
 
 	return ans
